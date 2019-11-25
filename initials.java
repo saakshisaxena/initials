f98d016525a78fbbe6af52a079092e6e9784f181
saakshisaxena <saakshi.saxena2000@gmail.com>
2019-11-25 22:31:52 +0000

    Add files via upload

---
+++ initials.java
@@ -0,0 +1,31 @@
+	/* Author : Saakshi Saxena
+	To print the initials of my name in patterns!*/
+
+	class initials  
+	{
+		public static void main(String[] param)
+		{
+			Firstinitial();
+			Secondinitial();
+			System.exit(0);
+		}
+		
+	//To print the decreasing triangle with my first initial
+		public static void Firstinitial()
+		{
+			System.out.println("  S S S S S  \n S \n S \n  S S S S S  \n          S \n          S \n S S S S S\n");
+
+
+			return;
+		}
+
+	// To print a random pattern with the second initials of my name!
+		public static void Secondinitial()
+		{
+			
+			System.out.println("  S S S S S \n S \n S \n  S S S S S \n          S \n          S \n S S S S S");
+			return;
+		}
+	}
+
+
